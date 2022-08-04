22746b9901b0ad3efe6ee7b8fb490c26bef8ad18
junyou123 <55748761+junyou123@users.noreply.github.com>
2022-08-04 12:58:44 +0800

    Fixed indentation 2

--- milestone1/operate.py
+++ milestone1/operate.py
@@ -138,19 +138,19 @@ class Operate:
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                 # TODO: replace with your code to make the robot drive forward
-		        self.command['motion'] = [2, 0]
+                self.command['motion'] = [2, 0]
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
                 # TODO: replace with your code to make the robot drive backward
-		        self.command['motion'] = [-2, 0]
+                self.command['motion'] = [-2, 0]
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
                 # TODO: replace with your code to make the robot turn left
-            	self.command['motion'] = [0, 2]
+                self.command['motion'] = [0, 2]
 		    # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
                 # TODO: replace with your code to make the robot turn right
-            	self.command['motion'] = [0, -2]
+                self.command['motion'] = [0, -2]
 		    ####################################################
             # stop
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
